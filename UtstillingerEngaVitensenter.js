36f09166ef5f003b9448343f6829cf7b9f4d176a
Heneriko70 <hebea015@osloskolen.no>
2024-06-06 19:58:56 +0200

    Ferdig kode

--- UtstillingerEngaVitensenter.js
+++ UtstillingerEngaVitensenter.js
@@ -1,14 +1,3 @@
-// Henter inn variabelen for knappen som tar deg til utstillinger
-var tilbud_utstillinger_knappEl = document.querySelector("#tilbud_utstillinger_knapp");
-// Legger til en eventlistener som lytter etter klikk på tilbud_utstillinger_knapp og kjører funksjonen UtstillingerTilbud
-tilbud_utstillinger_knappEl.addEventListener("click", UtstillingerTilbud);
-
-// Funksjon som tar deg til UtstillingerEngaVitensenter.html
-function UtstillingerTilbud() {
-    // Sender brukeren til UtstillingerEngaVitensenter.html
-    window.location.href = "UtstillingerEngaVitensenter.html"
-}
-
 // Henter inn variabelen for bildet i headeren  
 var logoEl = document.querySelector("#logo");
 // Legger til en eventlisteners som lytter etter klikk og tastetrykk på logoen og kjører funksjonen tilbakeHjem
