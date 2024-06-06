11fa8ace618ecefc375f50b4d184849a6f32d0a6
Heneriko70 <120127344+Heneriko70@users.noreply.github.com>
2024-06-06 10:31:29 +0000

    Arbeid på skolen.

---
+++ EngaVitensenter.js
@@ -0,0 +1,6 @@
+var tilbud_utstillinger_knappEl =document.querySelector("#tilbud_utstillinger_knapp");
+tilbud_utstillinger_knappEl.addEventListener("click",UtstillingerTilbud)
+
+function UtstillingerTilbud(){
+    window.location.href="UtstillingerEngaVitensenter.html"
+}
